598ca379be994172885839ddcc57001d4b1210c6
loesch94 <samuel.loescher@gmail.com>
2015-08-21 22:59:22 +0100

    Initial commit

---
+++ desktop/src/ludumdare/_33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ludumdare._33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ludumdare._33.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
---
+++ tests/src/ludumdare/_33/ExampleTests.java
@@ -0,0 +1,14 @@
+package ludumdare._33;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
