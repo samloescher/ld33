3cb50ab4745bd75063021d0ee0190fc67af92626
Lawrence Chan <lawrence.chan03@gmail.com>
2015-08-22 20:48:59 +0100

    Add initial background music.

--- core/src/ludumdare/_33/MainGame.java
+++ core/src/ludumdare/_33/MainGame.java
@@ -2,6 +2,7 @@ package ludumdare._33;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -9,20 +10,28 @@ import ludumdare._33.screens.MainMenuScreen;
 
 public class MainGame extends Game {
 	public static MainGame instance;
-	
+
 	public SpriteBatch batch;
-	public BitmapFont font ;
+	public BitmapFont font;
+	public Music backgroundMusic;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("fonts/I-pixel-u.fnt"),false);
+		font = new BitmapFont(Gdx.files.internal("fonts/I-pixel-u.fnt"), false);
+		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("sounds/happy-pixel-adventure.mp3"));
+		
+		backgroundMusic.setVolume(0.1f);
+		backgroundMusic.setLooping(true);
+		backgroundMusic.play();
+
 		instance = this;
 		setScreen(new MainMenuScreen());
 	}
-	
-	public void dispose(){
+
+	public void dispose() {
 		batch.dispose();
 		font.dispose();
+		backgroundMusic.dispose();
 	}
 }
