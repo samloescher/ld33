bdaff6d1f4c3aaf008a05ddd7ae629c13dfe140b
Lawrence Chan <lawrence.chan03@gmail.com>
2015-08-22 16:34:23 +0100

    Add basic menu.

--- core/src/ludumdare/_33/MainGame.java
+++ core/src/ludumdare/_33/MainGame.java
@@ -1,27 +1,30 @@
 package ludumdare._33;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
-public class MainGame extends ApplicationAdapter {
+public class MainGame extends Game {
 	SpriteBatch batch;
-	Texture img;
-	
+	BitmapFont font;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		/* Render everything in create() */
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 }
