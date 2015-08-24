763ac74e442f5792388b2fda6829cef1bc0aaba0
Lawrence Chan <lawrence.chan03@gmail.com>
2015-08-24 10:34:48 +0100

    Initial particle effects.

--- core/src/ludumdare/_33/particles/ParticleManager.java
+++ core/src/ludumdare/_33/particles/ParticleManager.java
@@ -1,5 +1,14 @@
 package ludumdare._33.particles;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+
 public class ParticleManager {
-	
+	public ParticleEffect particleEffect;
+
+	public ParticleManager() {
+		particleEffect = new ParticleEffect();
+		particleEffect.load(Gdx.files.internal("particles/fire-test"), Gdx.files.internal("particles"));
+	}
+
 }
